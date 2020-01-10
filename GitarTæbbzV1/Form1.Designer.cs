911aae9ebdda4774d71e5cec4ae55b1f129ef9fa
Karl Martin T.Jensen <kama.teje@gmail.com>
2020-01-10 01:09:53 +0100

    g

---
+++ GitarTæbbzV1/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace GitarTæbbzV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(313, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "o";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(345, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "o";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "o";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.BackColor = System.Drawing.Color.Transparent;
+            this.label16.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(313, 228);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(26, 27);
+            this.label16.TabIndex = 17;
+            this.label16.Text = "o";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.BackColor = System.Drawing.SystemColors.Control;
+            this.label17.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(345, 228);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(26, 27);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "o";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.Color.Transparent;
+            this.label18.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(377, 228);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(26, 27);
+            this.label18.TabIndex = 15;
+            this.label18.Text = "o";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(313, 202);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 27);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "o";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(345, 202);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(26, 27);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "o";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.Transparent;
+            this.label15.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(377, 204);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(26, 27);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "o";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(313, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 27);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "o";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(345, 176);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 27);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "o";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(377, 177);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 27);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "o";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(313, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 27);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "o";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(345, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 27);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "o";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(377, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 27);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "o";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(313, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 27);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "o";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(345, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "o";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(377, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 27);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "o";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KnappTrykk);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ GitarTæbbzV1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.IO;
+using System.Diagnostics;
+namespace GitarTæbbzV1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int selected = 1;
+        string[] a = File.ReadAllLines("tab.txt");
+        int posx1;
+
+        int str1Skille1 = 20;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            posx1 = 234 + 12;
+            SelectLabel();
+            LoadStrings();
+            
+        }
+        private void LoadStrings()
+        {
+            label1.Text = a[0].Substring(0, str1Skille1);
+            label2.Text = a[0].Substring(str1Skille1, 1);
+            label3.Text = a[0].Substring(str1Skille1 + 1);
+
+            label4.Text = a[1].Substring(0, str1Skille1);
+            label5.Text = a[1].Substring(str1Skille1, 1);
+            label6.Text = a[1].Substring(str1Skille1 + 1);
+
+            label7.Text = a[2].Substring(0, str1Skille1);
+            label8.Text = a[2].Substring(str1Skille1, 1);
+            label9.Text = a[2].Substring(str1Skille1 + 1);
+
+            label10.Text = a[3].Substring(0, str1Skille1);
+            label11.Text = a[3].Substring(str1Skille1, 1);
+            label12.Text = a[3].Substring(str1Skille1 + 1);
+
+            label13.Text = a[4].Substring(0, str1Skille1);
+            label14.Text = a[4].Substring(str1Skille1, 1);
+            label15.Text = a[4].Substring(str1Skille1 + 1);
+
+            label16.Text = a[5].Substring(0, str1Skille1);
+            label17.Text = a[5].Substring(str1Skille1, 1);
+            label18.Text = a[5].Substring(str1Skille1 + 1);
+
+            label1.Location = new Point(posx1 - 11 * label1.Text.Length, label1.Location.Y);
+            label4.Location = new Point(posx1 - 11 * label1.Text.Length, label4.Location.Y);
+            label7.Location = new Point(posx1 - 11 * label1.Text.Length, label7.Location.Y);
+            label10.Location = new Point(posx1 - 11 * label1.Text.Length, label10.Location.Y);
+            label13.Location = new Point(posx1 - 11 * label1.Text.Length, label13.Location.Y);
+            label16.Location = new Point(posx1 - 11 * label1.Text.Length, label16.Location.Y);
+        }
+
+
+        private void SelectLabel()
+        {
+            label17.BackColor = Color.Transparent;
+            label14.BackColor = Color.Transparent;
+            label11.BackColor = Color.Transparent;
+            label8.BackColor = Color.Transparent;
+            label5.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            switch (selected)
+            {
+                case 1:
+                    label17.BackColor = Color.DeepSkyBlue;
+                    break;
+                case 2:
+                    label14.BackColor = Color.DeepSkyBlue;
+                    break;
+                case 3:
+                    label11.BackColor = Color.DeepSkyBlue;
+                    break;
+                case 4:
+                    label8.BackColor = Color.DeepSkyBlue;
+                    break;
+                case 5:
+                    label5.BackColor = Color.DeepSkyBlue;
+                    break;
+                case 6:
+                    label2.BackColor = Color.DeepSkyBlue;
+                    break;
+            }
+        }
+
+        private void KnappTrykk(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                if (selected < 6)
+                    selected++;
+            if (e.KeyCode == Keys.Down)
+                if (selected > 1)
+                    selected--;
+            if (e.KeyCode == Keys.Right)
+                str1Skille1++;
+            if (e.KeyCode == Keys.Left)
+                str1Skille1--;
+
+
+            LoadStrings();
+
+            SelectLabel();
+        }
+
+    }
+}
